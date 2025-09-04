7b57faf8e27ffb9f858761fcd08e9f3106808048
orhunakkan <orhunakkan@outlook.com>
2025-09-03 23:13:33 -0500

    refactor: reorganize and uncomment code for clarity

--- 001 - JavaScript Fundamentals/Scripts/script.js
+++ 001 - JavaScript Fundamentals/Scripts/script.js
@@ -1,29 +1,55 @@
-let js = 'amazing';
-// if (js === 'amazing') alert('JavaScript is fun');
-console.log(40 + 10 - 20);
+// let js = 'amazing';
+// // if (js === 'amazing') alert('JavaScript is fun');
+// console.log(40 + 10 - 20);
+//
+// console.log('Jonas');
+//
+// let firstName = 'Orhun';
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+//
+// let orhun_akkan = 'OA';
+// console.log(orhun_akkan);
+// // let function = 10; -> Not allowed
+//
+// let isJsFun = true;
+// console.log(isJsFun);
+// console.log(typeof isJsFun);
+// console.log(typeof firstName);
+//
+// isJsFun = 'YES!'
+// console.log(isJsFun);
+//
+// let year;
+// console.log(year);
+// console.log(typeof year);
+//
+// // Bug
+// console.log(typeof null);
 
-console.log('Jonas');
+// let age = 30;
+// age = 31;
+//
+// // const needs initial value
+// const birthYear = 1990;
 
-let firstName = 'Orhun';
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
+const now = 2037;
+const ageOrhun = now - 1990;
+const ageRabia = now - 1992;
+console.log(ageOrhun, ageRabia);
 
-let orhun_akkan = 'OA';
-console.log(orhun_akkan);
-// let function = 10; -> Not allowed
+console.log(ageOrhun * 2, ageRabia / 2, 2 ** 3);
 
-let isJsFun = true;
-console.log(isJsFun);
-console.log(typeof isJsFun);
-console.log(typeof firstName);
+let x = 10 + 5;
+x += 10; // x = x + 10
+x *= 5; // x = x * 5
+x++;
+x--;
+console.log(x);
 
-isJsFun = 'YES!'
-console.log(isJsFun);
+// Comparison Operator
 
-let year;
-console.log(year);
-console.log(typeof year);
+console.log(ageOrhun > ageRabia);
+console.log(ageOrhun === ageRabia);
 
-// Bug
-console.log(typeof null);
