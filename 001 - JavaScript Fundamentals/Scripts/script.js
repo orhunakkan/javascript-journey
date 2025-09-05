92c18ce27417968c002f0682246a31c893647557
orhunakkan <orhunakkan@outlook.com>
2025-09-04 23:52:26 -0500

    coding challenges added

--- 001 - JavaScript Fundamentals/Scripts/script.js
+++ 001 - JavaScript Fundamentals/Scripts/script.js
@@ -1,38 +1,38 @@
-// let js = 'amazing';
-// // if (js === 'amazing') alert('JavaScript is fun');
-// console.log(40 + 10 - 20);
-//
-// console.log('Jonas');
-//
-// let firstName = 'Orhun';
-// console.log(firstName);
-// console.log(firstName);
-// console.log(firstName);
-//
-// let orhun_akkan = 'OA';
-// console.log(orhun_akkan);
-// // let function = 10; -> Not allowed
-//
-// let isJsFun = true;
-// console.log(isJsFun);
-// console.log(typeof isJsFun);
-// console.log(typeof firstName);
-//
-// isJsFun = 'YES!'
-// console.log(isJsFun);
-//
-// let year;
-// console.log(year);
-// console.log(typeof year);
-//
-// // Bug
-// console.log(typeof null);
+let js = 'amazing';
+// if (js === 'amazing') alert('JavaScript is fun');
+console.log(40 + 10 - 20);
 
-// let age = 30;
-// age = 31;
-//
-// // const needs initial value
-// const birthYear = 1990;
+console.log('Jonas');
+
+let firstName = 'Orhun';
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let orhun_akkan = 'OA';
+console.log(orhun_akkan);
+// let function = 10; -> Not allowed
+
+let isJsFun = true;
+console.log(isJsFun);
+console.log(typeof isJsFun);
+console.log(typeof firstName);
+
+isJsFun = 'YES!'
+console.log(isJsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+// Bug
+console.log(typeof null);
+
+let age = 30;
+age = 31;
+
+// const needs initial value
+const birthYear = 1990;
 
 const now = 2037;
 const ageOrhun = now - 1990;
